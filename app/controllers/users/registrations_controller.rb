d7bbf2fafed776990230e81ad90fb155967eb7ec
Oleksandr <53656177+Abitgor@users.noreply.github.com>
2024-02-06 14:20:10 +0200

    Set Up Device JWT Auth

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsStore
+  respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: 200,
+        message: 'Signed up successfully.',
+        email: current_user.email
+      }, status: :ok
+    else
+      render json: {
+        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
+  end
+end
