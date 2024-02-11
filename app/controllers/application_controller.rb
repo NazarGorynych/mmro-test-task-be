932f0029baeeba435258f7e551efaa88e95c9210
Oleksandr <53656177+Abitgor@users.noreply.github.com>
2024-02-11 21:04:04 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < ActionController::API
   before_action :raise_unauthorized_error
   before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :raise_unauthorized_error, if: :devise_controller?
+  after_action :run_auction_jobs
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+  end
+
+  def run_auction_jobs
+    UpdateAuctionStatusesJob.perform_later # temporary solution to not deploy cron job
   end
 
   private
