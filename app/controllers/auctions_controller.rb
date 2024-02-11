36f455d299b080af844f175087084157e4151c0c
Oleksandr <53656177+Abitgor@users.noreply.github.com>
2024-02-11 20:21:23 +0200

    fix

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AuctionsController < ApplicationController
-  included AuctionFiltersConcern
+  include AuctionFiltersConcern
   skip_before_action :raise_unauthorized_error
 
   def index
