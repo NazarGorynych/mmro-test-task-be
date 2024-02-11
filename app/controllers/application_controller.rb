8b42a3c27fd37b2dc87d17b6f7aa7162e1e202b2
Oleksandr <53656177+Abitgor@users.noreply.github.com>
2024-02-11 13:10:52 +0200

    add name and avatar to sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@
 class ApplicationController < ActionController::API
   respond_to :json
   before_action :raise_unauthorized_error
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+  end
 
   private
   def raise_unauthorized_error
